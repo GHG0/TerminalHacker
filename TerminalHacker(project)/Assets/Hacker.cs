a7408e5147626920db5d5c62af4b31170f76fce0
GHG0 <ddighe@cannonschool.org>
2020-10-01 09:23:52 -0400

    finished up to level 10

--- TerminalHacker(project)/Assets/Hacker.cs
+++ TerminalHacker(project)/Assets/Hacker.cs
@@ -8,20 +8,25 @@ public class Hacker : MonoBehaviour{
 		ShowMainMenu();
     }
     
-    //Mainmenue is asking what you would like to hack
+    //Mainmenu is asking what you would like to hack
     void ShowMainMenu(){
     	Terminal.ClearScreen();
-    	var greeting = "Hello Deven";
-    	Terminal.WriteLine(greeting);
-	    Terminal.WriteLine("What would you like to hack?");
+   	    Terminal.WriteLine("What would you like to hack?");
 	    Terminal.WriteLine("");
 	    Terminal.WriteLine("1 for Hospital");
 	    Terminal.WriteLine("2 for FBI");
 	    Terminal.WriteLine("3 for NSA");
-	    Terminal.WriteLine("Enter your selection below");	
+	    Terminal.WriteLine("Enter your selection below");
 	}
-    // Update is called once per frame
-    void Update(){
-        
-    }
+	//when the enter button is pressed
+	void OnUserInput(string input){
+		if(input == "menu"){
+			ShowMainMenu();
+		}
+		else{
+			Terminal.WriteLine("Please enter a valid level.");
+			
+		}
+	}
+
 }
