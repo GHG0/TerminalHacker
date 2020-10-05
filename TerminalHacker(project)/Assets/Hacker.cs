d8977380608c5c7dda7080cf013a221f7528db21
GHG0 <ddighe@cannonschool.org>
2020-10-05 09:37:46 -0400

    up to 16, before first solo

--- TerminalHacker(project)/Assets/Hacker.cs
+++ TerminalHacker(project)/Assets/Hacker.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Hacker : MonoBehaviour{
+	//gamestate
+	int level;
+	enum Screen {Main, Pass, Win}
+	Screen currentScreen;
     // Start is called before the first frame update
+    
+
     void Start(){
 		ShowMainMenu();
     }
@@ -18,15 +25,41 @@ public class Hacker : MonoBehaviour{
 	    Terminal.WriteLine("3 for NSA");
 	    Terminal.WriteLine("Enter your selection below");
 	}
-	//when the enter button is pressed
+	//when the button is pressed
 	void OnUserInput(string input){
-		if(input == "menu"){
+		if(input == "menu"){ //always go to menu
 			ShowMainMenu();
+			currentScreen = Screen.Main;
+		}
+		//setting the gamestate with input
+		else if (currentScreen == Screen.Main){
+			RunMainMenu(input);
+
+		}
+	}
+	void RunMainMenu(string input){
+		if(input == "1"){
+			level = 1;
+			StartGame();
+		}else if(input == "2"){
+			level = 2;
+			StartGame();
+		}else if(input == "3"){
+			level = 3;
+			StartGame();
 		}
 		else{
 			Terminal.WriteLine("Please enter a valid level.");
 			
 		}
+	}
+	
+	
+	void StartGame(){
+		currentScreen = Screen.Pass;
+		Terminal.WriteLine("you have chosen "+level);
+
+
 	}
 
 }
